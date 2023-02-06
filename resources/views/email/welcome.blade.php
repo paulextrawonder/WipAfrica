703146f2c8165482c4a2d4acf24650fb322eca31
paulextrawonder <paulextrawonder@gmail.com>
2023-02-06 16:16:13 +0100

    email fix

--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -2,9 +2,10 @@
 <strong>Hello {{$user['first_name']}},</strong>
 <br>Welcome to WBN Web App! 
 <br>We are thrilled to have you on board. 
-<br>We are confident that going on this journey with us will be economically transformative, financially enlightening, educationally informative and memorable for you. Be rest assured that it will change how you perceive, and make money. 
+<br><br>We are confident that going on this journey with us will be economically transformative, financially enlightening, educationally informative and memorable for you. 
+<br><br>Be rest assured that it will change how you perceive, and make money. 
 It’s time to get started. Everything you need to get started has been set up already, please click ‘WBN Mini-Academy’ to get started immediately. 
-<br>To further guide you, join our community of realtors here: WhatsApp and Telegram.
+<br><br>To further guide you, join our community of realtors here: WhatsApp and Telegram.
 Should you encounter any challenges, or have a question, please kindly reach out to us in the community or shoot us an email at comms@wipafrica.com 
 Welcome on board the Wealthy Flight!
 <br><br>...share the work, share the wealth and enjoy the freedom lifestyle...
